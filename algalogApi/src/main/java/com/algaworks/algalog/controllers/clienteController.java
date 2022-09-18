95169402b52458b84f7f6c718f5be10085c5f1aa
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-09-18 18:46:39 -0300

    Jakarta Implementation

--- algalogApi/src/main/java/com/algaworks/algalog/controllers/clienteController.java
+++ algalogApi/src/main/java/com/algaworks/algalog/controllers/clienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,31 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
-//@RequestMapping(value = "/clientes")
 public class clienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar(){
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setName("Fabiano Alexandre");
-		cliente1.setPhone("5845-5698");
-		cliente1.setEmail("fabiano@gamail.com");
-		var cliente2 = new Cliente();
-		cliente2.setId(1L);
-		cliente2.setName("Ruan");
-		cliente2.setPhone("5845-5698");
-		cliente2.setEmail("ruan@gamail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
-	}
-	
-//	@GetMapping
-//	public String listar() {
-//		return "Testando";
-//	}
-//	@GetMapping("/test")
-//	public String testar() {
-//		return "Testando caminho teste";
-//	}
+		return manager.createQuery("from Cliente",Cliente.class )
+				.getResultList();
+	}	
+
 }
