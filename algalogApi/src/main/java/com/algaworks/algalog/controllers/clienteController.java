299cee04961538e7bb1d4d466af46d81a9024d2c
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-09-19 00:04:57 -0300

    using Lombok notation @AllArgsConstructor

--- algalogApi/src/main/java/com/algaworks/algalog/controllers/clienteController.java
+++ algalogApi/src/main/java/com/algaworks/algalog/controllers/clienteController.java
@@ -2,17 +2,18 @@ package com.algaworks.algalog.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class clienteController {
 	
-	@Autowired
 	private ClienteRepository clienteRepository;
 
 	@GetMapping("/clientes")
