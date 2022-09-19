b1f3ebd54d6c5415e9c986867dc8204daf005a76
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-09-19 01:21:13 -0300

    ResponseEntity method

--- algalogApi/src/main/java/com/algaworks/algalog/controllers/clienteController.java
+++ algalogApi/src/main/java/com/algaworks/algalog/controllers/clienteController.java
@@ -1,14 +1,18 @@
 package com.algaworks.algalog.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 import lombok.AllArgsConstructor;
+
 
 @AllArgsConstructor
 @RestController
@@ -28,4 +32,13 @@ public class clienteController {
 	public List<Cliente> list(){
 		return clienteRepository.findByNomeContaining("Ru");
 	}
+	@GetMapping("/clientes/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+		}
 }
